487f06bc3b251741525fc77b37104a3b9cfb6348
Mudaliar <lingkesh.mudaliar@capgemini.com>
2020-10-09 16:05:41 +0530

    updated

--- src/store/modules/actions.js
+++ src/store/modules/actions.js
@@ -1,6 +1,19 @@
-import { getRandomDogImage} from '../../dog.service';
+import {getAllBreeds, getDogImage, getRandomDogImage} from '../../dog.service';
 
 const actions = {
+
+    async getAllBreedContent( {commit} ){
+        return getAllBreeds()
+        .then((res) =>
+            commit('SET_ALLBREEDS', res.data.message))
+            .catch((error) => console.log(`There was a problem getting the breeds: ${error.message}`))
+        },
+    
+    async getDogImages( {commit},dogName,count){
+        return getDogImage(dogName,count)
+        .then((res) => commit('SET_IMAGES', res.data))
+        .catch((error) => console.log(`There was a problem getting the images: ${error.message}`))
+    },
 
     async getRandomImage({commit}, dogName){
         return getRandomDogImage(dogName)
--- src/store/modules/mutations.js
+++ src/store/modules/mutations.js
@@ -1,5 +1,14 @@
 const mutations = {
     
+    
+    SET_ALLBREEDS(state, allBreeds){
+        state.allBreeds = allBreeds;
+    },
+
+    SET_IMAGES(state, imageUrl){
+        state.imageUrl = imageUrl;
+    },
+    
     SET_RANDOM(state, randomImageUrl){
         //console.log(randomImageUrl)
         state.randomImageUrl = randomImageUrl["message"];
